1a5932bb017a5033533834c501c1ed90354756f8
nino846272 <nino846272@gmail.com>
2026-06-02 21:59:50 +0600

    feat: integrate Vercel Analytics into main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { LanguageProvider } from './context/LanguageContext'
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <LanguageProvider>
       <App />
     </LanguageProvider>
